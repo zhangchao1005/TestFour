5b234927e3bcda87a2991377aabaf4fe67714397
Lee <email@exam>
2016-03-28 16:41:12 -0600

    lihang=按钮

--- app/src/main/java/com/lanou/dllo/testfour/MainActivity.java
+++ app/src/main/java/com/lanou/dllo/testfour/MainActivity.java
@@ -2,12 +2,23 @@ package com.lanou.dllo.testfour;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn = (Button) findViewById(R.id.btn);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "2358735735fgh25fg5h4725", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
