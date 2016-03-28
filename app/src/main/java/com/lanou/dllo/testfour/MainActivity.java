f53f786402eb43c4665699884dc7a4a1120b9d7b
dllo <1917143414@qq.com>
2016-03-28 17:14:31 +0800

    登陆完成

--- app/src/main/java/com/lanou/dllo/testfour/MainActivity.java
+++ app/src/main/java/com/lanou/dllo/testfour/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    //成功添加登陆功能
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
